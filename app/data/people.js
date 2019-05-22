87e5f5bf84d9412e11d041d3f0f08592b7bf11ef
Your NameGreg <you@example.comwatkinsgljr@gmail.com>
2019-05-21 20:48:46 -0500

    added constructer function and empty array

---
+++ app/data/people.js
@@ -0,0 +1,11 @@
+
+
+reservationArray = [];
+
+var Reservation = function (name, email, phone, date, time) {
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    this.date = date;
+    this.time = time;
+};
